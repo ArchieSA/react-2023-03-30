875a415f9d7c80610f5d60e68186f2b7f72c88fa
Konstantin Kiselev <kiselev.k@sparklingtide.com>
2023-04-08 23:56:45 +0300

    add Menu and Reviews to the Restaurant

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,18 +1,22 @@
-import { Button } from "@/components/Button/Button";
 import React from "react";
+import {Menu} from "../Menu/Menu";
+import styles from './Restaurant.module.css'
+import {Reviews} from "../Reviews/Reviews";
 
 export const Restaurant = ({ restaurant }) => {
   if (!restaurant) {
     return null;
   }
 
-  const { name } = restaurant;
+  const { name, menu, reviews } = restaurant;
 
   return (
-    <div>
-      <Button>
-        <span>{name}</span>
-      </Button>
+    <div className={styles.card}>
+        <h1 className={styles.name}>{name}</h1>
+        <hr className={styles.hr}/>
+        <Menu menu={menu}/>
+        <hr className={styles.hr}/>
+        <Reviews reviews={reviews}/>
     </div>
   );
 };

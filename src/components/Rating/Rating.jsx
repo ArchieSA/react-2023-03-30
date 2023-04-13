a8fd1ee5bc810ef76f4ec05c8125edbb09fa4d4e
M P <potyomkin.m@gmail.com>
2023-04-13 16:53:40 +0200

    task2 solution update

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from "classnames";
+import Image from 'next/image';
 
 import styles from "./styles.module.sass";
 
@@ -13,16 +14,20 @@ export const Rating = ({ratingCurrent, ratingMax}) => {
   let arr = [];
   for (let index = 0; index < ratingMax; index++) {
     if (index < ratingCurrent) {
-      arr[index] = 'star';
+      arr[index] = 1;
     } else {
-      arr[index] = 'nostar';
+      arr[index] = 0 ;
     }
     
   }
 
   return (
       <ul className={classNames(styles.rating)}>
-        {arr.map((value, index) => <li key={index} className={classNames(ratingStyles[value])}></li>)}
+        { arr.map((value, index) => (
+          <li key={index} >
+            {value ? <Image src="/images/star-gold.png" alt="" width="20" height="20" /> : <Image src="/images/star.png" alt="" width="20" height="20" /> }
+          </li>)
+        )}
       </ul>
   )
 }

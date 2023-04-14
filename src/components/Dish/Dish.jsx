de7446ddaed1ec3da6b141a0e903ed4e33c56dba
Dmitry Egorov <info@consultapp.ru>
2023-04-14 10:52:04 +0400

    hw3 Dish count + Rating click fn

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,25 +1,39 @@
 import { Button } from "@/components/Button/Button";
-import React from "react";
-
-import ThumbDown from "./images/thumb-down.svg";
+import React, { useState } from "react";
 
 import styles from "./styles.module.scss";
 
+const MAX_COUNT = 5;
+
 export const Dish = ({ dish }) => {
+  const [count, setCount] = useState(0);
+
   if (!dish) {
     return null;
   }
 
-  const { name, ingredients } = dish;
+  const { name } = dish;
 
   return (
     <div>
       <span>{name}</span>
-      <Button className={styles.decrementAction} type="secondary">
+      <Button
+        className={styles.decrementAction}
+        onClick={() => {
+          count > 0 && setCount(count - 1);
+        }}
+        type="secondary"
+      >
         -
       </Button>
-      0
-      <Button className={styles.incrementAction} type="primary">
+      {count}
+      <Button
+        className={styles.incrementAction}
+        onClick={() => {
+          count < MAX_COUNT && setCount(count + 1);
+        }}
+        type="primary"
+      >
         +
       </Button>
     </div>

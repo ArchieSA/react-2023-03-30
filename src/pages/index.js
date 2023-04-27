27401fe5130fa33d3d2ecc7a5cd2e4751690d3a6
Boris Kaminskiy <93283934+BorisKaminskiy@users.noreply.github.com>
2023-04-27 12:21:37 +0300

    Delete src directory

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/Button/Button";
-import { Home } from "@/components/Home/Home";
-import React from "react";
-
-export default function HomePage() {
-  return <Home />;
-}
--- src/store/entities/dish/selectors.js
+++
@@ -1,4 +0,0 @@
-export const selectDishModule = (state) => state.dish;
-
-export const selectDishById = (state, { dishId }) =>
-  selectDishModule(state).entities[dishId];

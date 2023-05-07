42bfb307af063689fcf6bf4fc7ef6ae91ef3acb5
Alex <black.s.w.a.n@yandex.ru>
2023-05-07 16:24:40 +0300

    lect9

--- src/containers/Cart/Cart.jsx
+++ src/containers/Cart/Cart.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { useSelector } from "react-redux";
-import { Cart } from "@/components/Cart/Cart";
-import { selectCartModule } from "@/store/ui/cart/selectors";
-
-export const CartContainer = () => {
- 
-  const cartState = useSelector(selectCartModule);
-
-  return <Cart cartState={cartState} />;
-=======
 import { Cart } from "@/components/Cart/Cart";
 import { selectCartModule } from "@/store/ui/cart/selectors";
 import React from "react";
@@ -23,5 +11,4 @@ export const CartContainer = () => {
   }
 
   return <Cart cart={cart} />;
->>>>>>> 24831b1ccea2d39ee5c37958571f20f48499064f
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import { dishReducer } from "@/store/entities/dish";
-import { usersReducer } from "@/store/entities/users";
-import { restaurantReducer } from "@/store/entities/restaurant";
-import { reviewsReducer } from "@/store/entities/reviews";
-=======
 import { dishReducer, dishSlice } from "@/store/entities/dish";
 import { restaurantReducer } from "@/store/entities/restaurant";
 import { reviewReducer } from "@/store/entities/review";
 import { userReducer } from "@/store/entities/user";
 import { logger } from "@/store/middlewares/logger";
->>>>>>> 24831b1ccea2d39ee5c37958571f20f48499064f
 import { cartReducer } from "@/store/ui/cart";
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
@@ -17,15 +10,9 @@ import { combineReducers } from "redux";
 const rootReducer = combineReducers({
   cart: cartReducer,
   restaurant: restaurantReducer,
-<<<<<<< HEAD
-  dish: dishReducer,
-  reviews: reviewsReducer,
-  user: usersReducer,
-=======
   dish: dishSlice.reducer,
   review: reviewReducer,
   user: userReducer,
->>>>>>> 24831b1ccea2d39ee5c37958571f20f48499064f
 });
 
 export const store = configureStore({
